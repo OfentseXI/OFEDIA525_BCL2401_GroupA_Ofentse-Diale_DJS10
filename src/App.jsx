8fddc4850e0b65c9495d6f90bf5a748324d04ad9
OfentseXI <dofentse69@gmail.com>
2024-06-14 14:39:21 +0200

    code cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -26,24 +26,22 @@ export default function App() {
  
   if (error) {
     return ( <div className = "error-message">
-      <h1>Failed to fetch blog posts</h1>
+      <h1>Data fetching failed</h1>
     </div>
     );
   }
-  
-  return (     <
-    div className="app-container">       
-    <h1>Posts</h1> {/Main heading for the blog posts/}       
-    <div className="blog-posts">         
-    {posts.map((post, index) => (           
-    <div key={post.id} className="post">            
-    {/* Display the post title with a sequential number*/}             
-    <h2>               {index + 1}.{post.title}             </h2> 
-                {/ Display the post body */}  
-                           <p>{post.body}</p>  
-                                    </div>      
-                                       ))}       
-                                       </div>     
-                                       </div>  
-                                       );
-                                       }
+
+  return (
+    <div className ="app-container">
+      <h1>Blog Posts</h1>
+      <div className ="blog-posts">
+        {posts.map((post,index) => (
+          <div key = {post.id} className="post">
+            <h2>{index + 1}.{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+      ))}
+      </div>
+    </div>
+  );
+}
