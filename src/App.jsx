f8931ab03089781cab9bb6c54069a0b4eb76a090
OfentseXI <dofentse69@gmail.com>
2024-06-14 14:05:47 +0200

    Added useEffect hook to fetch data from API, handled errors, and rendered fetched data in the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 export default function App() {
@@ -6,5 +6,42 @@ export default function App() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState('null');
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
 
+        if (!response.ok) {
+          throw new Error("Failed to fetch blog posts");
+        }
+        const data = await response.json();
+        setPosts(data);
+      }
+      catch (error) {
+        setError(error.message);
+      }
+    };
+    fetchPosts();
+  }, []);
+ 
+  if (error) {
+    return ( <div className = "error-message">
+      <h1>Data fetching failed</h1>
+    </div>
+    );
+  }
+
+  return (
+    <div className ="app-container">
+      <h1>Blog Posts</h1>
+      <div className ="blog-posts">
+        {posts.map((post,index) => (
+          <div key = {post.id} className="post">
+            <h2>{index + 1}.{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+      ))}
+      </div>
+    </div>
+  );
 }
